d6132cbcd7e715b761a31dc203227181f706010e
Melissa Holmes <melissa@bignerdranch.com>
2013-11-16 15:33:20 -0500

    Create user model plue user and sessions controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChallegeMe::Application.config.secret_key_base = 'f4ed9f053bd05296ca8a64e8abf3fe6851fb8fcf3339e723acb9e89ef0eb2183c506450c6837ea00455d59edcb1c015cdc67fcddc82926473f5dbfc45aa7e195'
---
+++ db/migrate/20131116200111_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :uid
+      t.string :champion
+      t.integer :wins
+      t.integer :losses
+
+      t.timestamps
+    end
+  end
+end
