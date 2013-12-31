d98ce86516fcc7b1190993a311fee7d28bfecfe0
Melissa Holmes <meltar@gmail.com>
2013-12-31 13:36:08 -0500

    Display the winner and loser for recent contests
    * Add helper methods to display the name of the challenger and
    challenged users
    * Display "YOU" as the name if the user.name belongs to the current
    signed in user
    * Change the view to have an ordered list instead of unordered one

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,19 @@
 module UsersHelper
+	def winner_name(users, winner_id)
+		if current_user.id == winner_id
+			"YOU"
+		else
+			winner = users.find(winner_id)
+			winner.name
+		end
+	end
+
+	def loser_name(users, loser_id)
+		if current_user.id == loser_id
+			"YOU"
+		else
+			loser = users.find(loser_id)
+			loser.name
+		end
+	end
 end
